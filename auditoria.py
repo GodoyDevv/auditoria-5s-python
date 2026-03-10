e49f4ff0cac0bf4be01416adc324887520843895
Godoy <brunosgy2@gmail.com>
2026-03-09 23:33:56 -0300

    Adicionado contagem individual das auditorias

--- auditoria.py
+++ auditoria.py
@@ -1,114 +1,42 @@
 import pandas as pd
 
-dados = pd.read_excel("Auditoria.xlsx")
+# ler planilha
+df = pd.read_excel("auditoria5S.xlsx")
 
-print(dados)
+# percorrer cada auditoria (cada linha)
+for index, linha in df.iterrows():
 
-ruim = 0
-regular = 0
-bom = 0
-otimo = 0
-na = 0
+    ruim = 0
+    regular = 0
+    bom = 0
+    otimo = 0
+    na = 0
 
-for coluna in dados.columns:
-    
-    for resposta in dados[coluna]:
+    # pegar respostas das perguntas
+    respostas = linha[8:30]  # começa na pergunta 1
+
+    for resposta in respostas:
         
-        if resposta == "Ruim":
-            ruim += 1
-            
-        elif resposta == "Regular":
-            regular += 1
-            
-        elif resposta == "Bom":
-            bom += 1
-            
-        elif resposta == "Ótimo":
-            otimo += 1
-            
-        elif resposta == "NA":
+        if pd.isna(resposta):
             na += 1
 
-print("Resultado da Auditoria")
-print("Ruim:", ruim)
-print("Regular:", regular)
-print("Bom:", bom)
-print("Ótimo:", otimo)
-print("NA:", na)
+        elif resposta == "Ruim":
+            ruim += 1
+
+        elif resposta == "Regular":
+            regular += 1
+
+        elif resposta == "Bom":
+            bom += 1
+
+        elif resposta == "Ótimo":
+            otimo += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print("Sistema de Avaliação Auditoria 5S")
-
-# respostas = [
-# "Regular",
-# "Bom",
-# "Otimo",
-# "N/A",
-# ]
-
-# Ruim = 0
-# Regular = 0
-# Bom = 0
-# Otimo = 0
-# NA = 0
-
-# for resposta in respostas:
-
-#     if resposta == "Ruim":
-#         Ruim = Ruim + 1
-
-#     elif resposta == "Regular":
-#         Regular = Regular + 1
-
-#     elif resposta == "Bom":
-#         Bom = Bom + 1
-
-#     elif resposta == "Otimo":
-#         Otimo = Otimo + 1
-
-#     elif resposta == "N/A":
-#         NA = NA + 1
-
-# print("Resultado da Auditoria")
-# print("Ruim: ", Ruim)
-# print("Regular: ", Regular)
-# print("Bom: ", Bom)
-# print("NA: ", NA)
-
-
-
+    print("Auditoria", index + 1)
+    print("Ruim:", ruim)
+    print("Regular:", regular)
+    print("Bom:", bom)
+    print("Ótimo:", otimo)
+    print("N/A:", na)
+    print("---------------------")
---
+++ rodar_auditoria.bat
@@ -0,0 +1,2 @@
+python auditoria.py
+pause
